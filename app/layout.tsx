3e30d0f84cf9f79e835a750ed692c06398198b6d
Rama Novaris Ayyubi Pratama <ramaanovariss@gmail.com>
2023-02-01 08:45:48 +0800

    styling menu with tailwind css

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,36 +2,28 @@
 
 import Link from "next/link";
 import "../src/styles/globals.css";
-import { useRouter } from "next/navigation";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
-
-  const onClickUrl = (url: string) => {
-    router.push(url);
-  };
+  const menuItem = (url: string, title: string, className?: string) => (
+    <Link className="no-underline" href={url}>
+      <div className={`bg-black text-white py-2 px-3 rounded-lg ${className}`}>{title}</div>
+    </Link>
+  );
 
   return (
     <html>
       <head />
-      <body>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link href="/setting">Setting</Link>
-          </li>
-          <li>
-            <Link href="/calculator">Calculator</Link>
-          </li>
-          <li>
-            <Link href="/cari">Cari Orang</Link>
-          </li>
-        </ul>
+      <body className="container mx-auto py-[10px]">
+        {/* Menu */}
+        <div className="inline-flex w-full">
+          {menuItem("/", "Home", "bg-white text-blue-500 text-lg")}
+          <div className="inline-flex justify-end space-x-3 w-full">
+            {menuItem("/dashboard", "Dashboard")}
+            {menuItem("/setting", "Setting")}
+            {menuItem("/calculator", "Calculator")}
+            {menuItem("/cari", "Cari")}
+          </div>
+        </div>
         {children}
       </body>
     </html>
